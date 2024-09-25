91084ad542c4f0357ede8b2ff3ecacb7cb3b8c19
Anirudh <anirudh.singh@thoughtworks.com>
2024-09-25 08:08:33 +0530

    Refactor BAB

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,10 +1,9 @@
 import {
-    MaterialCommunityIcons,
+  MaterialCommunityIcons,
 } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { CommonActions } from "@react-navigation/native";
 import React from 'react';
-import { BottomNavigation } from "react-native-paper";
+import BottomAppBar from "../components/BottomAppBar";
 import AllExpenses from "./AllExpenses";
 import RecentExpenses from "./RecentExpenses";
 
@@ -16,47 +15,7 @@ const Home = () => {
       screenOptions={{
         headerShown: false,
       }}
-      tabBar={({ navigation, state, descriptors, insets }) => (
-        <BottomNavigation.Bar
-          navigationState={state}
-          safeAreaInsets={insets}
-          onTabPress={({ route, preventDefault }) => {
-            const event = navigation.emit({
-              type: "tabPress",
-              target: route.key,
-              canPreventDefault: true,
-            });
-
-            if (event.defaultPrevented) {
-              preventDefault();
-            } else {
-              navigation.dispatch({
-                ...CommonActions.navigate(route.name, route.params),
-                target: state.key,
-              });
-            }
-          }}
-          renderIcon={({ route, focused, color }) => {
-            const { options } = descriptors[route.key];
-            if (options.tabBarIcon) {
-              return options.tabBarIcon({ focused, color, size: 24 });
-            }
-
-            return null;
-          }}
-          getLabelText={({ route }) => {
-            const { options } = descriptors[route.key];
-            const label =
-              options.tabBarLabel !== undefined
-                ? options.tabBarLabel
-                : options.title !== undefined
-                ? options.title
-                : route.title;
-
-            return label;
-          }}
-        />
-      )}
+      tabBar={(data) => BottomAppBar(data)}
     >
       <Tab.Screen
         name="Recent"
