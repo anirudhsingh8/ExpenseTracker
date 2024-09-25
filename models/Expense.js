dd96e0b718367cff3e0f9503be2b5d1563333cfb
Anirudh <anirudh.singh@thoughtworks.com>
2024-09-25 07:14:58 +0530

    Add expense model

---
+++ models/Expense.js
@@ -0,0 +1,20 @@
+class Expense {
+    constructor(data) {
+        this.id = data.id;
+        this.title = data.title;
+        this.amount = data.amount;
+        this.date = new Date(data.date);
+        this.notes = data.notes;
+    }
+
+    // Method to display expense details
+    displayExpense() {
+        return `
+        Expense ID: ${this.id}
+        Title: ${this.title}
+        Amount: ${this.amount} ${this.currency}
+        Date: ${this.date.toLocaleDateString()}
+        Notes: ${this.notes}
+      `;
+    }
+}
